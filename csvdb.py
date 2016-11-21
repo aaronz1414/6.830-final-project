d56585cd8da3023d37ffae9d5578bcfbd9a8c7b6
Jordan Lucier <jordan@jordanlucier.com>
2016-11-21 01:56:23 -0500

    beginning stuff

--- csvdb.py
+++ csvdb.py
@@ -1,24 +1,34 @@
-import csv
+import sqlite3 as sqlite
+import csv, os
+
 from csvtable import Table
 
-def create_table(table):
-	pass
-
 def main():
+	os.remove('csvdb.db')
+	conn = sqlite.connect('csvdb.db')
+	cur = conn.cursor()
 	print "CSVDB console"
 	filename = raw_input("Enter the path to your csv: ")
 	with open(filename, 'r') as f:
 		table = None
 		reader = csv.reader(f, delimiter=',')
 		for r in reader:
-			table = Table(r)
-			break
+			if not table:
+				table = Table(r, name=filename.strip('.csv'))
+				table.create_table(cur)
+				conn.commit()
+			
+			table.insert_tuple(cur, r)
+		conn.commit()
+	print 'done'
+	test(table.name)
 
-		print table.types
-		# reader = csv.reader(f, delimiter=',')
-		# for r in reader:
-
-
+def test(name):
+	conn = sqlite.connect('csvdb.db')
+	cur = conn.cursor()
+	for r in cur.execute('select * from {}'.format(name)):
+		print r
+	print 'test done'
 
 if __name__ == '__main__':
 	main()
--- csvtable.py
+++ csvtable.py
@@ -1,5 +1,10 @@
 import uuid
-import sqlite3
+
+TYPE_MAP = {
+	str : 'text',
+	int : 'real',
+	float : 'real'
+}
 
 def get_type(data):
 	try:
@@ -18,14 +23,29 @@ def get_type(data):
 
 class Table(object):
 	def __init__(self, row, name=None):
-		tmptypes = list()
-		for x in row:
-			tmptypes.append(get_type(x))
-
-		self.types = tuple(tmptypes)
+		self.types = tuple([get_type(x) for x in row])
 		self.name = name or str(uuid.uuid4())
-		self.colnames = tuple([self.name + '-' + str(i)
+		self.colnames = tuple([self.name + '_' + str(i)
 			for i in xrange(len(self.types))])
 
 	def __str__(self):
 		return "<" + self.name + " : " + str(self.colnames) + ">"
+
+	def create_string(self):
+		type_str = '('
+		for i in xrange(len(self.types)):
+			type_str += '{} {},'.format(self.colnames[i],
+				TYPE_MAP[self.types[i]])
+		type_str = type_str.strip(',')
+		type_str += ')'
+
+		return 'CREATE TABLE {} {}'.format(self.name, type_str)
+
+	def create_table(self, cur):
+		cur.execute(self.create_string())
+
+	def insert_tuple(self, cur, tup):
+		if len(tup) != len(self.types):
+			raise ValueError("Invalid tuple")
+
+		cur.execute('INSERT INTO {} VALUES {}'.format(self.name, tuple(tup)))
