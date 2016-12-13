0595df129d7f3445d40b3198c22a43567979cd30
Jordan Lucier <jordan@jordanlucier.com>
2016-12-13 15:55:36 -0500

    File monitoring

--- csvdb.py
+++ csvdb.py
@@ -1,32 +1,60 @@
 import sqlite3 as sqlite
-import csv, os
+import csv, os, time
 
 from csvtable import Table
+from monitor import Monitor
+
+class CSVDB(object):
+	def __init__(self, filename):
+		try:
+			os.remove('csvdb.db')
+		except:
+			pass
+		self.filename = filename
+		self.monitor = Monitor(self.filename, self._db_setup, self.update_db)
+		self.file = open(self.filename, 'r')
+		self.table = None
+
+	def start(self):
+		self.monitor.run()
+
+	def stop(self):
+		self.monitor.stop()
+
+	def _db_setup(self):
+		self.conn = sqlite.connect('csvdb.db')
+		self.cur = self.conn.cursor()
+		self.update_db()
+
+	def update_db(self):
+		self.cur.execute('DROP TABLE IF EXISTS ' + self.filename.strip('.csv') + ';')
+		reader = csv.reader(self.file, delimiter=',')
+		for r in reader:
+			if not self.table:
+				self.table = Table(r, name=self.filename.strip('.csv'))
+				self.table.create_table(self.cur)
+				self.conn.commit()
+
+			self.table.insert_tuple(self.cur, r)
+		self.conn.commit()
 
 def main():
-	os.remove('csvdb.db')
-	conn = sqlite.connect('csvdb.db')
-	cur = conn.cursor()
 	print "CSVDB console"
 	filename = raw_input("Enter the path to your csv: ")
-	with open(filename, 'r') as f:
-		table = None
-		reader = csv.reader(f, delimiter=',')
-		for r in reader:
-			if not table:
-				table = Table(r, name=filename.strip('.csv'))
-				table.create_table(cur)
-				conn.commit()
-			
-			table.insert_tuple(cur, r)
-		conn.commit()
+	db = CSVDB(filename)
+	db.start()
 	print 'done'
-	test(table.name)
+	try:
+		raw_input('waiting...')
+	except:
+		print 'terminating...'
+	# test(db)
+	db.stop()
 
-def test(name):
+def test(db):
 	conn = sqlite.connect('csvdb.db')
 	cur = conn.cursor()
-	for r in cur.execute('select * from {}'.format(name)):
+	for r in cur.execute('select * from {}'.format(db.table.name)):
 		print r
 	print 'test done'
 
--- monitor.py
+++ monitor.py
@@ -5,53 +5,27 @@ from threading import Thread
 from csvtable import Table
 
 class Monitor(object):
-
-	def __init__(self):
-		os.remove('csvdb.db')
-		self.conn = sqlite.connect('csvdb.db')
-		self.cur = self.conn.cursor()
-		self.filename = ''
+	def __init__(self, filename, setup, callback):
+		self.filename = filename
 		self.table = None
 		self.last_file_mod = 0
+		self.setup = setup
+		self.callback = callback
+		self.kill = False
 
 	def run(self):
-		self.filename = raw_input("Enter the path to your csv: ")
-		self.update_db()
-		self.test(self.table.name)
-
 		self.last_file_mod = os.stat(self.filename).st_mtime
-		self.monitor_file_change_in_background()
+		Thread(target=self.monitor_file_change_in_background).start()
 
-	def update_db(self):
-		self.cur.executescript('DROP TABLE IF EXISTS ' + self.filename.strip('.csv') + ';')
-		self.table = None
-		with open(self.filename, 'r') as f:
-			reader = csv.reader(f, delimiter=',')
-			for r in reader:
-				if not self.table:
-					self.table = Table(r, name=self.filename.strip('.csv'))
-					self.table.create_table(self.cur)
-					self.conn.commit()
-				
-				if len(r) == 0:
-					continue
-				self.table.insert_tuple(self.cur, r)
-			self.conn.commit()
+	def stop(self):
+		self.kill = True
 
 	def monitor_file_change_in_background(self):
-		while True:
+		self.setup()
+		while not self.kill:
 			if os.stat(self.filename).st_mtime > self.last_file_mod:
 				self.last_file_mod = os.stat(self.filename).st_mtime
-				self.update_db()
+				print '\nfile changed\n'
+				self.callback()
+				print 'back to waiting...'
 			time.sleep(1)
-
-
-	def test(self, name):
-		self.conn = sqlite.connect('csvdb.db')
-		self.cur = self.conn.cursor()
-		for r in self.cur.execute('select * from {}'.format(name)):
-			print r
-		print 'test done'
-
-monitor = Monitor()
-monitor.run()
